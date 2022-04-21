27f341fd97f507fcf861b8a46b1745ecc58799d9
zerodoc <danielc@smallwoodhome.com>
2022-04-21 14:39:34 -0500

    remove sqlite schemas

--- scheme/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  username TEXT NOT NULL,
-  created_at INTEGER DEFAULT 0,
-  config_name TEXT,
-  config_version TEXT,
-
-  FOREIGN KEY(config_version, config_name) REFERENCES configs(version, name)
-);
